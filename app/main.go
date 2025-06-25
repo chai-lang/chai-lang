50f27ceac2037d06dea347d01d65f1f3ce97fbe8
Dhruva <rdhruva12345@gmail.com>
2025-06-25 14:34:28 +0530

    feat: add token type

--- app/main.go
+++ app/main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"chai-lang/app/scanner"
 	"fmt"
+	"os"
 )
 
 func main() {
-	scanner := &scanner.Scanner{}
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Println("Invalid usage\nRun ./run.sh tokenize filename.chai")
+		return
+	}
+	command := args[1]
 
-	err := scanner.Scan()
+	if command != "tokenize" {
+		fmt.Println("Invalid usage\nRun ./run.sh tokenize filename.chai")
+		return
+	}
+
+	filename := args[2]
+	rawContent, err := os.ReadFile(filename) // command, ./run.sh tokenize test.chai
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	scanner := scanner.NewScanner(rawContent)
+	err = scanner.Scan()
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
--- app/scanner/main.go
+++ app/scanner/main.go
@@ -1,6 +1,58 @@
 package scanner
 
+type TokenType string
+
+const (
+	// Single character
+	LEFT_PAREN  TokenType = "LEFT_PAREN"
+	RIGHT_PAREN TokenType = "RIGHT_PAREN"
+	LEFT_BRACE  TokenType = "LEFT_BRACE"
+	RIGHT_BRACE TokenType = "RIGHT_BRACE"
+	COMMA       TokenType = "COMMA"
+	SEMICOLON   TokenType = "SEMICOLON"
+	DOT         TokenType = "DOT"
+	PLUS        TokenType = "PLUS"
+	MINUS       TokenType = "MINUS"
+	STAR        TokenType = "STAR"
+	EOF         TokenType = "EOF"
+
+	// One or two
+	BANG          TokenType = "BANG"       // !
+	BANG_EQUAL    TokenType = "BANG_EQUAL" // !=
+	EQUAL         TokenType = "EQUAL"
+	EQUAL_EQUAL   TokenType = "EQUAL_EQUAL"   // ==
+	GREATER       TokenType = "GREATER"       // >
+	LESS          TokenType = "LESS"          // <
+	GREATER_EQUAL TokenType = "GREATER_EQUAL" // >=
+	LESS_EQUAL    TokenType = "LESS_EQUAL"    // <=
+	SLASH         TokenType = "SLASH"
+	NUMBER        TokenType = "NUMBER"
+	IDENTIFIER    TokenType = "IDENTIFIER"
+
+	// Keywords
+	AND      TokenType = "AND"
+	OR       TokenType = "OR"
+	IF       TokenType = "IF"
+	ELIF     TokenType = "ELIF"
+	ELSE     TokenType = "ELSE"
+	WHILE    TokenType = "WHILE"
+	FOR      TokenType = "FOR"
+	FUNCTION TokenType = "FUNCTION"
+	RETURN   TokenType = "RETURN"
+	PRINT    TokenType = "PRINT"
+	TRUE     TokenType = "TRUE"
+	FALSE    TokenType = "FALSE"
+	NIL      TokenType = "NIL"
+)
+
 type Scanner struct {
+	source []byte
+}
+
+func NewScanner(source []byte) *Scanner {
+	return &Scanner{
+		source: source,
+	}
 }
 
 func (s *Scanner) Scan() error {
