b54bda0f9eb80eb34fbc08e152566a5e4bdaded6
Shivang Rathore <rshivang12345@gmail.com>
2025-06-30 10:13:59 +0530

    feat(interpreter): add comparision

--- internal/interpreter/interpreter.go
+++ internal/interpreter/interpreter.go
@@ -49,7 +49,18 @@ func (i *Interpreter) VisitBinaryExpr(expr *ast.BinaryExpr) any {
 		return left.(float64) * right.(float64)
 	case ast.SLASH:
 		return left.(float64) / right.(float64)
-	// TODO: Add missing binary operations
+	case ast.GREATER:
+		return left.(float64) > right.(float64)
+	case ast.GREATER_EQUAL:
+		return left.(float64) >= right.(float64)
+	case ast.LESS:
+		return left.(float64) < right.(float64)
+	case ast.LESS_EQUAL:
+		return left.(float64) <= right.(float64)
+	case ast.EQUAL_EQUAL:
+		return i.isEqual(left, right)
+	case ast.BANG_EQUAL:
+		return !i.isEqual(left, right)
 	default:
 		// Unreachable code
 		return nil
@@ -68,6 +79,22 @@ func (i *Interpreter) isTruthy(value any) bool {
 		return boolean
 	}
 	return true
+}
+
+func (i *Interpreter) isEqual(left, right any) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if leftType, ok := left.(string); ok {
+		if rightType, ok := right.(string); ok {
+			return leftType == rightType
+		}
+		return false
+	}
+	return left == right
 }
 
 func (i *Interpreter) Interpret(tree ast.Expr) any {
